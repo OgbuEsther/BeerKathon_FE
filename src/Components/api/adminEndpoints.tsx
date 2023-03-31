19b083d445e02295e3cdd6cb939237a179dd23d3
nicsylvia <nicsylvia15f@gmail.com>
2023-03-31 01:17:22 -0700

    working on reading admin

--- src/Components/api/adminEndpoints.tsx
+++ src/Components/api/adminEndpoints.tsx
@@ -1,3 +1,10 @@
 import axios from "axios";
 
-const liveURL = "https://football-predict-api.onrender.com";
+const Endpoint = "https://football-predict-api.onrender.com";
+
+// Get single user/admin:
+export const SingleAdmin = async (userID: any) => {
+  return await axios
+    .get(`${Endpoint}/getsingleuser/${userID}`)
+    .then((res) => res.data);
+};
